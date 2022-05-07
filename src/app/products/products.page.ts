fdf2c7b58bac248e5827876405262ceab1e062be
DanielFitzsimons <80958065+DanielFitzsimons@users.noreply.github.com>
2022-05-07 21:22:06 +0100

    third

---
+++ src/app/products/products.page.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController, NavController } from '@ionic/angular';
+import { CartService } from '../Services/cart.service';
+import { BehaviorSubject } from 'rxjs';
+import { CartModalPage } from '../pages/cart-modal/cart-modal.page';
+
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.page.html',
+  styleUrls: ['./products.page.scss'],
+})
+export class ProductsPage implements OnInit {
+
+  cart = [];
+  products = [];
+  numCartItems: BehaviorSubject<number>;
+
+  constructor(private navCtrl:NavController, private cartService: CartService, private modalCtrl: ModalController) { }
+
+  ngOnInit() {
+    this.products = this.cartService.getProducts();
+    this.cart = this.cartService.getCart();
+    this.numCartItems = this.cartService.getNumCartItems();
+  }
+
+  addToCart(product)
+  {
+    this.cartService.addProduct(product);
+  }
+
+  async openCart()
+  {
+      let modal = await this.modalCtrl.create({
+        component:  CartModalPage,
+        cssClass: 'cart-modal'
+      });
+      modal.present();
+
+  }
+
+ 
+
+}
+
