ac9d1b74ced14e3ea0489f67d3207e5be8e1a116
DanielFitzsimons <80958065+DanielFitzsimons@users.noreply.github.com>
2022-05-07 21:05:08 +0100

    first

---
+++ src/app/Services/cart.service.ts
@@ -0,0 +1,96 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+export interface Product{
+  id: number;
+  title: string;
+  name:string;
+  price: number;
+  amount: number;
+  
+
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  data1: Product[] = [
+
+    {id: 0, title: 'Thriller', name: 'Michael Jackson', price: 9.95, amount: 10},
+    {id: 1, title: 'Back in Black', name: 'AC/DC', price: 8.95, amount: 10},
+    {id: 2, title: 'The Dark side of the Moon', name: 'Pink Floyd', price: 6.95, amount: 10},
+    {id: 3, title: 'The Bodyguard', name: 'Whitney Heuston', price: 9.95, amount: 10},
+    {id: 4, title: 'Bad', name: 'Michael Jackson', price: 9.95, amount: 10},
+  ];
+
+private cart = [];
+private numCartItems = new BehaviorSubject(0);
+
+constructor() { }
+
+getProduct()
+{
+  return this.data1;
+}
+
+getCart()
+{
+  return this.cart;
+}
+
+getNumCartItems()
+{
+  return this.numCartItems;
+}
+
+addProduct(product)
+{
+  let added = false;
+  for(let p of this.cart)
+  {
+    if(p.id == product.id){
+      p.amount += 1;
+      added =true;
+      break;
+    }
+    if(!added)
+    {
+      product.amount =1;
+      this.cart.push(product);
+    }
+    this.numCartItems.next(this.numCartItems.value + 1);
+  }
+}
+
+decreaseProduct(product)
+{
+  for(let [index, p] of this.cart.entries())
+  {
+    if(p.id === product.id)
+    {
+      p.amount -=1;
+      if(p.amount ==0)
+      {
+        this.cart.splice(index, 1);
+      }
+    }
+  }
+  this.numCartItems.next(this.numCartItems.value - 1)
+}
+
+deleteProduct(product)
+{
+  for(let [index, p] of this.cart.entries())
+  {
+  if([p.id === product.id])
+    {
+      this.numCartItems.next(this.numCartItems.value - p.amount);
+      this.cart.splice(index, 1);
+    }
+  }
+}
+
+  
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ArtistsService} from '../Services/artists.service';
+import { ModalController, NavController } from '@ionic/angular';
+import { CartService } from '../Services/cart.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+artistData:any = [];
 
-  constructor() {}
 
+cart = [];
+products = [];
+numCartItems: BehaviorSubject<number>;
+
+  constructor(private artistsService:ArtistsService, private navCtrl:NavController, private cartService: CartService, private modalCtrl: ModalController) {}
+
+  
+    
+  subsribe(){
+    this.navCtrl.navigateBack('/subscribe');
+  }
+
+
+  feedback(){
+    this.navCtrl.navigateBack('/feedback');
+  }
+
+  shop(){
+    this.navCtrl.navigateBack('/products');
+  }
+
+  ngOnInit() {
+    this.artistsService.getArtistsData().subscribe(
+      (data)=>{
+        this.artistData = data.Artists;
+        console.log(this.artistData); 
+      }
+    );
+
+
+    
+
+
+   
+
+  }
+
+
+  
 }
